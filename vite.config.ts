aad74c0893f81d1948caff5859b53e513e5442f6
voltria <trungnguyen.31241022633@st.ueh.edu.vn>
2025-12-18 15:39:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   },
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY),
-    'process.env.GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY),
+    'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY)
   },
   resolve: {
     alias: {
